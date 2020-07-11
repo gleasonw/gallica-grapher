f34a85a7a8fe3dbe5d6515aa9b99353f355d4c14
unknown <gleasonw@carleton.edu>
2020-07-11 09:44:31 -0700

    Tidied everything up, fixed funky XML parsing, ready for R integration.

--- gallicaHunter.py
+++ gallicaHunter.py
@@ -21,14 +21,15 @@ class GallicaHunter:
     def __init__(self, searchTerm, recordNumber, newspaper, yearRange):
         self.searchTerm = searchTerm
         self.recordNumber = recordNumber
-        self.journalDateIdentifierResults = []
+        self.dateJournalIdentifierResults = []
         self.newspaper = ''
         self.yearRange = []
         self.numberQueriesToGallica = 10
+        self.query = ''
         self.establishNewspaperDictionary(newspaper)
         self.establishYearRange(yearRange)
         self.establishNumberQueries(recordNumber)
-
+        self.establishQuery()
 
     """
     params:
@@ -39,49 +40,36 @@ class GallicaHunter:
 
     """
     def hunt(self):
-        #Add something to restrict year range if wanted
         for newspaper in self.newspaperDictionary:
             newspaperKey = self.newspaperDictionary[newspaper]
             startRecord = 0
-
             for j in range(self.numberQueriesToGallica):
-                parameters = {"version": 1.2, "operation": "searchRetrieve", "query" :'arkPress all "%s" and (gallica all "%s") sortby dc.date/sort.ascending' % (newspaperKey, self.searchTerm), "startRecord" : startRecord, "maximumRecords" : 50, "collapsing" : "disabled"}
+                queryWithProperPaper = self.query.format(newsKey = newspaperKey)
+                parameters = {"version": 1.2, "operation": "searchRetrieve", "query" : queryWithProperPaper, "startRecord" : startRecord, "maximumRecords" : 50, "collapsing" : "disabled"}
                 response = requests.get("https://gallica.bnf.fr/SRU",params=parameters)
                 root = etree.fromstring(response.content)
-
-                # Format everything for eventual csv copy
-                startOfResultsList = len(self.journalDateIdentifierResults)
-
-                # Go through XML, add all journal titles (all entries). Then go through dates, adding to index incrementally (date in same order as title). Ditto for identifier.
-                for child in root.iter("{http://purl.org/dc/elements/1.1/}title"):
-                    journalTitle = child.text
-                    self.journalDateIdentifierResults.append(journalTitle)
-                i = startOfResultsList
-                for child in root.iter("{http://purl.org/dc/elements/1.1/}date"):
-                    journalDate = child.text
-                    journalTitle = self.journalDateIdentifierResults[i]
-                    journalTitleDate = "{0}, {1}".format(journalTitle, journalDate)
-                    self.journalDateIdentifierResults[i] = journalTitleDate
-                    i = i + 1
-                i = startOfResultsList
-                for child in root.iter("{http://purl.org/dc/elements/1.1/}identifier"):
-                    journalTitleDate = self.journalDateIdentifierResults[i]
-                    journalIdentifier = child.text
-                    journalTitleDateIdentifier = "{0}, {1}".format(journalTitleDate, journalIdentifier)
-                    self.journalDateIdentifierResults[i] = journalTitleDateIdentifier
-                    i = i + 1
+                for queryHit in root.iter("{http://www.loc.gov/zing/srw/}record"):
+                    data = queryHit[2][0]
+                    dateOfHit = data.find('{http://purl.org/dc/elements/1.1/}date').text
+                    journalOfHit = data.find('{http://purl.org/dc/elements/1.1/}title').text
+                    identifierOfHit = data.find('{http://purl.org/dc/elements/1.1/}identifier').text
+                    fullResult = dateOfHit + ", " + journalOfHit + ", " + identifierOfHit
+                    currentResults = self.dateJournalIdentifierResults
+                    currentResults.append(fullResult)
                 startRecord = startRecord + 51
+            #run same but don't check the range each time
 
-        filePacker = GallicaPackager(self.searchTerm, self.newspaper, self.journalDateIdentifierResults)
+        filePacker = GallicaPackager(self.searchTerm, self.newspaper, self.dateJournalIdentifierResults, self.yearRange)
         filePacker.makeCSVFile()
 
     def establishNewspaperDictionary(self,newspaper):
         fullNewspaperDictionary = GallicaHunter.newspaperDict
-        if newspaper is None:
+        if (newspaper is None) or (newspaper == "all"):
             self.newspaperDictionary = fullNewspaperDictionary
         else:
             trimmedDictionary = {newspaper : fullNewspaperDictionary[newspaper]}
             self.newspaperDictionary = trimmedDictionary
+            self.newspaper = newspaper
 
     def establishYearRange(self, yearRange):
         self.yearRange = re.split(r'[;,\-\s*]', yearRange)
@@ -89,3 +77,11 @@ class GallicaHunter:
     def establishNumberQueries(self, recordNumber):
         if recordNumber is not None:
             self.numberQueriesToGallica = recordNumber // 50
+
+    def establishQuery(self):
+        if(len(self.yearRange) == 0):
+            self.query = 'arkPress all "{{newsKey}}" and (gallica all "{searchWord}") sortby dc.date/sort.ascending'.format(searchWord = self.searchTerm)
+        else:
+            lowerYear = self.yearRange[0]
+            higherYear = self.yearRange[1]
+            self.query = '(dc.date >= "{firstYear}" and dc.date <= "{secondYear}") and ((arkPress all "{{newsKey}}") and (gallica all "{searchWord}")) sortby dc.date/sort.ascending'.format(firstYear = lowerYear, secondYear = higherYear, searchWord = self.searchTerm)
--- gallicaPackager.py
+++ gallicaPackager.py
@@ -1,26 +1,33 @@
 import os.path
 
 class GallicaPackager:
-    def __init__(self, searchTerm, newspaper, csvEntries):
-        self.searchTerm = searchTerm
-        self.csvEntries = csvEntries
-        self.newspaper = newspaper
+    def __init__(self, searchTerm, newspaper, csvEntries, yearRange):
+        self.querySearchTerm = searchTerm
+        self.querycsvEntries = csvEntries
+        self.queryNewspaper = newspaper
+        self.queryYearRange = yearRange
 
     def makeCSVFile(self):
         fileName = self.determineFileName()
         outFile = open(fileName, "w")
         outFile.write("journal,date,url\n")
-        for csvEntry in self.csvEntries:
+        for csvEntry in self.querycsvEntries:
             outFile.write(csvEntry + "\n")
         outFile.close()
 
     def determineFileName(self):
-        if self.newspaper is None:
+        if (self.queryNewspaper is None) or (self.queryNewspaper == "all"):
             nameOfFile = "AllNewspapers--"
         else:
-             nameOfFile = self.newspaper + "--"
-             wordsInQuery = self.searchTerm.split(" ")
+             nameOfFile = self.queryNewspaper + "--"
+             wordsInQuery = self.querySearchTerm.split(" ")
+
         for word in wordsInQuery:
             nameOfFile = nameOfFile + word
+
+        if len(self.queryYearRange) != 0:
+            lowerYear = self.queryYearRange[0]
+            higherYear = self.queryYearRange[1]
+            nameOfFile = nameOfFile + " " + lowerYear + "-" + higherYear
         nameOfFile = nameOfFile + ".csv"
         return(nameOfFile)
