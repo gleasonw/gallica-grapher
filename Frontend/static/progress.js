d9ad560b2e359f99bce3b48c3150ba3099683139
Will <gleasonw@carleton.edu>
2020-12-08 17:02:50 -0800

    a working progress bar... very exciting

--- Frontend/static/progress.js
+++ Frontend/static/progress.js
@@ -1,15 +1,15 @@
+
+
 function updateProgress(threadId){
     worker();
     function worker() {
         $.get('getProgress/' + threadId,function (data) {
             var progress = Number(data);
-            console.log(progress);
-            if(progress < 100){
-                $( "#progressbar" ).progressbar({
-                    value: progress
-                });
-                setTimeout(worker, 500);
+            console.log(progress)
+            if(progress <= 100){
+                $( "#progressBar" ).progressbar( "value", progress );
             }
+            setTimeout(worker, 1000);
         });
     }
 }
