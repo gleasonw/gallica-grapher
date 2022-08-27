7bdc6dbc3f20229851a2b5b775b3bd81f6361755
Will Gleason <wgleason9@gmail.com>
2022-08-27 15:45:53 -0600

    heroku backend deploy

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -1,4 +1,4 @@
 import os
 
-result_backend = os.environ.get('CELERY_BACKEND', 'redis://localhost:6379/0')
+result_backend = os.environ.get('UPSTASH_REDIS', 'redis://localhost:6379/0')
 broker_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672//')
