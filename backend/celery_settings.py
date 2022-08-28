27061d0c4623a18357135ad4a9fbf1fa9154d4dc
Will Gleason <wgleason9@gmail.com>
2022-08-28 13:58:56 -0600

    upstash

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -1,7 +1,6 @@
 import os
 import ssl
 
-broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_NONE}
 redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_NONE}
 result_backend = os.environ.get('UPSTASH_REDIS_URL', 'redis://localhost:6379/0')
 broker_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672//')
