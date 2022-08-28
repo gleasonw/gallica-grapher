babe3b1fe6acae9efd72ce159fdcce5a2564506b
Will Gleason <wgleason9@gmail.com>
2022-08-28 15:50:03 -0600

    broker change

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -2,7 +2,7 @@ import os
 
 result_backend = os.environ.get(
     'REDIS_URL',
-    "localhost:6379")
+    "redis://localhost:6379")
 broker_url = os.environ.get(
-    'CLOUDAMQP_URL',
-    'amqps://ajcbnuig:9K8W9c8J1z1w3f7Js-evF2yMlo_wCLrk@stingray.rmq.cloudamqp.com/ajcbnuig')
+    'REDIS_URL',
+    'redis://localhost:6379')
