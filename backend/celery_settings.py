c54bd93abb2efc3ad2be05d258a8262b1774d7b2
Will Gleason <wgleason9@gmail.com>
2022-08-28 13:52:06 -0600

    upstash

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -1,4 +1,8 @@
 import os
+import ssl
 
+broker_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE
+}
 result_backend = os.environ.get('UPSTASH_REDIS_URL', 'redis://localhost:6379/0')
 broker_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672//')
