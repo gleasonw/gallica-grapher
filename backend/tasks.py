918e134807f10554dc35f04b1d560d5061f5af50
Will Gleason <wgleason9@gmail.com>
2022-09-03 10:40:08 -0700

    remove memory monitoring

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,7 +1,6 @@
 from celery import Celery
 from scripts.request import Request
 import time
-import psutil, os
 
 app = Celery()
 app.config_from_object('celery_settings')
@@ -12,9 +11,6 @@ def spawnRequest(self, tickets):
     gallicaRequest = Request(tickets, spawnRequest.request.id)
     gallicaRequest.start()
     while True:
-        # print how much memory the process is using in MB
-        process = psutil.Process(os.getpid())
-        print(process.memory_info().rss / 1024 / 1024)
         if gallicaRequest.finished:
             return {
                 'status': 'Complete!',
