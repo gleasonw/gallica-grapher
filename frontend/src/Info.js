06d82dcc1dae506ec74c7424d99b4f7c2b615520
Will Gleason <wgleason9@gmail.com>
2022-09-20 10:34:21 -0700

    more memory for celery workers

--- frontend/src/Info.js
+++ frontend/src/Info.js
@@ -25,13 +25,13 @@ export default function Info(props){
                 </section>
                 <h3>Do I need to speak French to use it?</h3>
                 <section>
-                    <p>Most of the periodicals in the archive are in French, and hence French words are most relevant. There are, though, a sizeable number of international or
+                    <p>Most of the periodicals in the archive are in French, so French words are most relevant. There are, though, a sizeable number of international or
                         European editions in English, such as the New York Herald. Play around with it!</p>
                 </section>
                 <section>
                     <p>Pour les francophones qui se demandent pourquoi ce site est en anglais : je vais traduire le site bientôt, je me concentre sur le système pour l'instant.</p>
                 </section>
-                <h3> How do I began a search? </h3>
+                <h3> How do I begin a search? </h3>
                 <section>
                     <ol>
                         <li>Enter a term into the search bar;</li>
@@ -42,7 +42,7 @@ export default function Info(props){
                         <br/>
                         <li>Click the 'Fetch and Graph 📊' button;</li>
                         <br/>
-                        <li>Watch the results roll in. If your term is especially popular, I would recommend a cup of tea. Requests are run concurrently, but to avoid
+                        <li>Watch the results roll in. If your term is especially popular, I would recommend a cup of tea. Requests run concurrently, but to avoid
                             blasting the French national library's servers, I have erred on a smaller, more polite number of worker threads.</li>
                     </ol>
                 </section>
@@ -57,8 +57,8 @@ export default function Info(props){
                     Beneath the chart is a table of the records pulled from Gallica, with links to the scanned periodical. Clicking on
                     a point in the chart will list the corresponding records. You can also make direct requests by entering
                     a date above the table. I'm still designing this section for better usability -- I think it's the most
-                    interesting part of the tool. You can visually peruse cultural trends as they arise, then dive into the texts that
-                    provide deeper context.
+                    interesting part of the tool. You can see cultural trends as they arise, then dive into the texts that
+                    provide context.
                     </p>
                 </section>
                 <h3>Can I see the code?</h3>
