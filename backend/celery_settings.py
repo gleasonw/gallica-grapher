3bd6dccf0dc9ee14d07a25dbe7efb31123f7a4b5
Will Gleason <wgleason9@gmail.com>
2022-09-24 12:34:10 -0700

    redis buildpacksL

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -3,6 +3,7 @@ import ssl
 
 broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
 redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
+
 result_backend = os.environ.get(
     'REDIS_URL',
     "redis://localhost:6379")
