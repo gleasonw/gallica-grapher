644d725f74ff40b08c7be15a41e6c733569e1ad0
Will Gleason <wgleason9@gmail.com>
2022-09-24 08:56:56 -0700

    stunnel

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -1,8 +1,8 @@
 import os
 import ssl
 
-broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_NONE}
-redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_NONE}
+broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
+redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
 result_backend = os.environ.get(
     'REDIS_URL',
     "redis://localhost:6379")
