fc90df144c4c8b52674e3bddc467e523104480b8
Will Gleason <wgleason9@gmail.com>
2022-09-24 12:35:51 -0700

    redis ssl tinkering

--- backend/celery_settings.py
+++ backend/celery_settings.py
@@ -1,8 +1,8 @@
 import os
 import ssl
 
-broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
-redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_REQUIRED}
+broker_use_ssl = {'ssl_cert_reqs': ssl.CERT_OPTIONAL}
+redis_backend_use_ssl = {'ssl_cert_reqs': ssl.CERT_OPTIONAL}
 
 result_backend = os.environ.get(
     'REDIS_URL',
