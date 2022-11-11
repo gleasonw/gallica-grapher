a178ac2d09d42b07049b700f3622b533cf750f7a
gleasonw <wgleason9@gmail.com>
2022-11-11 13:21:41 -0800

    re upping files... why did I add that to gitignore

---
+++ backend/gallicaGetter/fetch/get.py
@@ -0,0 +1,48 @@
+import time
+from urllib3.util.retry import Retry
+import urllib3
+
+
+class Response:
+
+    def __init__(self, data, query, elapsed):
+        self.xml = data
+        self.query = query
+        self.elapsed = elapsed
+
+
+class Get:
+
+    def __init__(self, baseUrl, maxSize=50):
+        self.baseUrl = baseUrl
+        self.maxSize = maxSize
+        self.http = self.buildUrllib3()
+
+    def buildUrllib3(self):
+        retryStrategy = Retry(
+            status_forcelist=[500, 502, 503, 504],
+            backoff_factor=1,
+        )
+        http = urllib3.PoolManager(
+            timeout=urllib3.Timeout(connect=30, read=123),
+            retries=retryStrategy,
+            maxsize=self.maxSize
+        )
+        return http
+
+    def get(self, query) -> Response:
+        start = time.perf_counter()
+        response = self.http.request(
+            "GET",
+            self.baseUrl,
+            fields=query.getFetchParams()
+        )
+        end = time.perf_counter()
+        if response.status != 200:
+            print(f"Gallica HTTP response Error: {response.status}")
+        return Response(
+            data=response.data,
+            query=query,
+            elapsed=end - start
+        )
+
---
+++ backend/gallicaGetter/parse/date.py
@@ -0,0 +1,60 @@
+import re
+import weakref
+
+
+class Date:
+
+    _cache = {}
+    dateFormats = [
+        re.compile(r"^\d{4}-\d{1,2}-\d{1,2}$"),
+        re.compile(r"^\d{4}-\d{1,2}$"),
+        re.compile(r"^\d{4}$")
+    ]
+
+    @staticmethod
+    def __new__(cls, dateText):
+        dateText = str(dateText)
+        selfref = Date._cache.get(dateText)
+        if not selfref:
+            self = super().__new__(cls)
+            self.dateText = dateText
+            self.date = Date.parseDateText(dateText)
+            Date._cache[dateText] = weakref.ref(self)
+        else:
+            self = selfref()
+        return self
+
+    @staticmethod
+    def parseDateText(dateText):
+        date = [None, None, None]
+        for dateFormat in Date.dateFormats:
+            if dateFormat.match(dateText):
+                for index, entry in enumerate(dateText.split('-')):
+                    date[index] = entry
+                return date
+        return date
+
+    def __init__(self, dateText):
+        pass
+
+    def __repr__(self):
+        return f'Date({self.date})'
+
+    def getDateAsList(self) -> list:
+        return self.date
+
+    def getDate(self) -> str:
+        return self.dateText
+
+    def getYear(self) -> str:
+        return self.date[0]
+
+    def getMonth(self) -> str:
+        return self.date[1]
+
+    def getDay(self) -> str:
+        return self.date[2]
+
+    def __del__(self):
+        del Date._cache[self.dateText]
+
