cab2551b3234bd9cde118e92e38222e92f42278d
gleasonw <wgleason9@gmail.com>
2022-11-13 16:58:51 -0800

    syntax

--- backend/appsearch/pyllicaWrapper.py
+++ backend/appsearch/pyllicaWrapper.py
@@ -4,18 +4,24 @@ from gallicaGetter.parse.groupedCountRecord import GroupedCountRecord
 
 
 def get(**kwargs):
-    convertedArgs = {}
-    convertedArgs['recherche'] = kwargs['terms']
-    convertedArgs['corpus'] = 'presse'
+    convertedArgs = {
+        'recherche': kwargs['terms'],
+        'corpus': 'presse'
+    }
     if start := kwargs.get('startDate'):
         convertedArgs['debut'] = Date(start).getYear()
     if end := kwargs.get('endDate'):
         convertedArgs['fin'] = Date(end).getYear()
     if kwargs.get('grouping') == 'year':
         convertedArgs['resolution'] = 'annee'
+    return convert_data_frame_to_grouped_record(
+        pyllica(**convertedArgs),
+        kwargs['ticketID'],
+        kwargs['requestID']
+    )
 
 
-def convert_data_frame_to_grouped_record(self, df, ticketID, requestID):
+def convert_data_frame_to_grouped_record(df, ticketID, requestID):
     rows = list(zip(df.date, df.ratio, df.gram))
     for row in rows:
         yield GroupedCountRecord(
