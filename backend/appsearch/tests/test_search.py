2f62fca8b62e5637d30867c19ef6ea864013b82f
Will Gleason <wgleason9@gmail.com>
2022-12-08 19:56:39 -0800

    more tests

--- backend/appsearch/tests/test_search.py
+++ backend/appsearch/tests/test_search.py
@@ -16,7 +16,6 @@ class Test(TestCase):
             grouping='all',
         )
         search.insert_records_into_db = MagicMock()
-
         # when
         search.get_and_insert_records_for_args(
             ticketID='test',
@@ -27,10 +26,49 @@ class Test(TestCase):
             api=MagicMock(),
         )
         # then
-        search.insert_records_into_db.assert_called_with(
-            ticketID='test',
-            records=MagicMock(),
-            conn=MagicMock(),
-            requestID='test',
+        search.insert_records_into_db.assert_called_once()
+
+    def test_period_occurrence_search(self):
+        # given
+        args = SearchArgs(
+            terms=['test'],
+            start_date='1860',
+            end_date='1880',
+            codes=['test'],
+            grouping='year',
         )
+        search.insert_records_into_db = MagicMock()
+        # when
+        search.get_and_insert_records_for_args(
+            ticketID='test',
+            requestID='test',
+            args=args,
+            onProgressUpdate=MagicMock(),
+            conn=MagicMock(),
+            api=MagicMock(),
+        )
+        # then
+        search.insert_records_into_db.assert_called_once()
+
+    def test_pyllica_search(self):
+        # given
+        args = SearchArgs(
+            terms=['test'],
+            start_date='1860',
+            end_date='1880',
+            codes=['test'],
+            grouping='year',
+        )
+        search.insert_records_into_db = MagicMock()
+        search.pyllicaWrapper = MagicMock()
+        # when
+        search.get_and_insert_records_for_args(
+            ticketID='test',
+            requestID='test',
+            args=args,
+            onProgressUpdate=MagicMock(),
+            conn=MagicMock(),
+        )
+        # then
+        search.insert_records_into_db.assert_called_once()
 
