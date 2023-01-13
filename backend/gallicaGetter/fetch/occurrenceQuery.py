fbb0d37454e4fd5ff87a9bc5495769b7b120a681
Will Gleason <wgleason9@gmail.com>
2023-01-13 11:15:03 -0800

    remove redundant model, make occurrence query base model

--- backend/gallicaGetter/fetch/occurrenceQuery.py
+++ backend/gallicaGetter/fetch/occurrenceQuery.py
@@ -1,10 +1,9 @@
 from typing import List, Optional
-from gallicaGetter.fetch.sruQueryMixin import SRUQueryMixin
-from dataclasses import dataclass
+
+from pydantic import BaseModel
 
 
-@dataclass(slots=True)
-class OccurrenceQuery(SRUQueryMixin):
+class OccurrenceQuery(BaseModel):
     term: str
     start_date: str
     end_date: str
@@ -16,9 +15,6 @@ class OccurrenceQuery(SRUQueryMixin):
     link_term: Optional[str] = ""
     link_distance: Optional[int] = 0
     collapsing = False
-
-    def __post_init__(self):
-        self.cql = self.generate_cql()
 
     def make_copy(self, start_index: int, num_records: int = 1):
         return OccurrenceQuery(
@@ -55,6 +51,25 @@ class OccurrenceQuery(SRUQueryMixin):
             return f'text adj "{self.term}"'
         else:
             return ""
+
+    def get_params_for_fetch(self):
+        base = {
+            "operation": "searchRetrieve",
+            "exactSearch": "True",
+            "version": 1.2,
+            "startRecord": self.start_index,
+            "maximumRecords": self.num_records,
+            "query": self.generate_cql(),
+            "collapsing": self.collapsing and "true" or "false",
+        }
+        return base
+
+    def build_periodical_cql(self):
+        if self.codes and self.codes[0]:
+            formatted_codes = [f"{code}_date" for code in self.codes]
+            return 'arkPress adj "' + '" or arkPress adj "'.join(formatted_codes) + '"'
+        else:
+            return 'dc.type all "fascicule" and ocr.quality all "Texte disponible"'
 
     def __repr__(self):
         return f"Occurrence Query ({self.cql})"
