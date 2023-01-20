bf68cfb26908e8ff83280d7f83208ed4ac779e1e
Will Gleason <wgleason9@gmail.com>
2023-01-20 14:04:24 -0800

    analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { trpc } from "../utils/trpc";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
 
 export default trpc.withTRPC(App);
