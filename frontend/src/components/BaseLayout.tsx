6acbac2c8816cf9df5edd8f3684415798bc74790
Will Gleason <wgleason9@gmail.com>
2023-03-10 17:13:38 -0800

    initial design (thanks Google trends)

--- frontend/src/components/BaseLayout.tsx
+++ frontend/src/components/BaseLayout.tsx
@@ -13,42 +13,41 @@ export const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
   const lang = useContext(LangContext);
   return (
     <div className="h-screen w-screen flex flex-col">
-      <div className={"flex flex-row items-center justify-between p-5 border-b bg-slate-100"}>
-        <Link href="/" className={"text-3xl self-start"}>
-          📊
-        </Link>
-        <div className={"flex flex-row gap-5 align-center justify-center"}>
-          <label className={"relative inline-flex items-center cursor-pointer"}>
-            <span className={"mr-3"}>fr</span>
-            <input
-              onChange={props.onToggleLang}
-              type="checkbox"
-              value=""
-              className={"sr-only peer"}
-            />
-            <div
-              className={
-                "relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"
-              }
-            ></div>
-            <span className={"ml-3"}>en</span>
-          </label>
-
-          <Link href="/info">
-            <Image src={"/info.svg"} width={40} height={40} alt={"Info page"} />
+      <div
+        className={
+          "flex flex-row items-center justify-between p-5 border-b-2"
+        }
+      >
+        <div className={"flex"}>
+          <Link className={"p-5 text-3xl"} href="/">
+            Gallica Grapher
           </Link>
+          <Link href="/" className={"p-5"}>
+            Graph
+          </Link>
+          <Link href="/explore" className={"p-5"}>
+            Context
+          </Link>
+
+          <Link href="/info" className={"p-5"}>
+            Info
+          </Link>
+        </div>
+        <div className={"flex flex-row gap-5 align-center justify-center"}>
+          <select
+            onChange={props.onToggleLang}
+            value={lang.lang}
+            className={""}
+          >
+            <option value="fr">Français</option>
+            <option value="en">English</option>
+          </select>
+
           <Link
             href={"https://github.com/gleasonw/gallica-grapher"}
             target={"_blank"}
           >
             <Image src={"/github.svg"} width={40} height={40} alt={"Github"} />
-          </Link>
-          <Link
-            target={"_blank"}
-            href="https://github.com/gleasonw/gallica-grapher/issues"
-            className={"text-center self-center w-10"}
-          >
-            Report bug
           </Link>
         </div>
       </div>
---
+++ frontend/src/pages/explore/index.tsx
@@ -0,0 +1,5 @@
+import { BaseLayout } from "../../components/BaseLayout";
+
+export default function Explore() {
+  return <BaseLayout>Explore</BaseLayout>;
+}
