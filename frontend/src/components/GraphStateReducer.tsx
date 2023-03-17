b31de2d0709723e515fdf1cb1a6ddc0f13038028
Will <wgleason9@gmail.com>
2023-03-17 16:02:12 -0700

    add missing types

--- frontend/src/components/GraphStateReducer.tsx
+++ frontend/src/components/GraphStateReducer.tsx
@@ -1,14 +1,5 @@
+import { Paper } from "../models/dbStructs";
 import { GraphTicket } from "../pages/GraphTicket";
-import {
-  AddTicketAction,
-  RemoveTicketAction,
-  SetYearRangeAction,
-  SetMonthAction,
-  SetGroupingAction,
-  SetSmoothingAction,
-  SetSelectedTicketAction,
-  RemoveExampleTicketsAction,
-} from "./reducerActions";
 
 export interface GraphPageState {
   tickets: GraphTicket[];
@@ -18,7 +9,54 @@ export interface GraphPageState {
   smoothing: number;
   selectedTicket?: number;
 }
+export interface AddPaperAction {
+  type: "add_paper";
+  payload: Paper;
+}
 
+export interface RemovePaperAction {
+  type: "remove_paper";
+  payload: Paper["code"];
+}
+
+export interface AddTicketAction {
+  type: "add_ticket";
+  payload: GraphTicket;
+}
+
+export interface RemoveTicketAction {
+  type: "remove_ticket";
+  payload: GraphPageState["selectedTicket"];
+}
+
+export interface RemoveExampleTicketsAction {
+  type: "remove_example_tickets";
+}
+
+export interface SetYearRangeAction {
+  type: "set_year_range";
+  payload: GraphPageState["yearRange"];
+}
+
+export interface SetMonthAction {
+  type: "set_month";
+  payload: GraphPageState["month"];
+}
+
+export interface SetGroupingAction {
+  type: "set_grouping";
+  payload: GraphPageState["grouping"];
+}
+
+export interface SetSmoothingAction {
+  type: "set_smoothing";
+  payload: GraphPageState["smoothing"];
+}
+
+export interface SetSelectedTicketAction {
+  type: "set_selected_ticket";
+  payload: GraphPageState["selectedTicket"];
+}
 export function graphStateReducer(
   state: GraphPageState,
   action:
