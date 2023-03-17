de8f6d7b13e603b7ca5febc31ac3379767daaf86
Will <wgleason9@gmail.com>
2023-03-17 16:15:15 -0700

    more type fixes

--- frontend/src/utils/addQueryParamsIfExist.ts
+++ frontend/src/utils/addQueryParamsIfExist.ts
@@ -4,15 +4,13 @@ export function addQueryParamsIfExist(
 ) {
   const urlParams = new URLSearchParams();
   Object.entries(params).forEach(([key, value]) => {
-    if (
-      value !== undefined &&
-      value !== null &&
-      !Array.isArray(value)
-    ) {
+    if (value !== undefined && value !== null && !Array.isArray(value)) {
       urlParams.append(key, value);
     } else if (Array.isArray(value)) {
       value.forEach((v) => {
-        urlParams.append(key, v);
+        if (typeof v === "string" && v.length > 0) {
+          urlParams.append(key, v);
+        }
       });
     }
   });
