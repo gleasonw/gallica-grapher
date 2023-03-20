d6ef25e0951ebcbaef0fbf09cfdac049fa9287b3
Will Gleason <wgleason9@gmail.com>
2023-03-20 14:24:04 -0700

    reduce gap size in dash layout

--- frontend/src/components/DashboardLayout.tsx
+++ frontend/src/components/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 export default function DashboardLayout(props: { children: React.ReactNode }) {
   return (
     <div className="ml-10 mr-10 mb-10">
-      <div className="justify-center items-center flex flex-col mt-10 gap-5 md:gap-10 lg:gap-10 xl:gap-10">
+      <div className="justify-center items-center flex flex-col mt-10 gap-5">
         {props.children}
       </div>
     </div>
