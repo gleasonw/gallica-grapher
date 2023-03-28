9b889235bfc7bfa42508da35e8237a6abfc9bb09
Will Gleason <wgleason9@gmail.com>
2023-03-28 13:06:27 -0700

    french info page

--- frontend/src/components/Info.tsx
+++ frontend/src/components/Info.tsx
@@ -1,9 +1,13 @@
-import Explain from "./assets/explain.mdx";
+import { useContext } from "react";
+import { LangContext } from "./LangContext";
+import EnExplain from "./assets/explain_en.mdx";
+import FrExplain from "./assets/explain_fr.mdx";
 
 export default function Info() {
+  const { lang } = useContext(LangContext);
   return (
     <article className="text-justify m-auto prose prose-lg p-2 lg:prose-lg xl:prose-2xl mt-20 mb-20">
-      <Explain />
+      {lang === "en" ? <EnExplain /> : <FrExplain />}
     </article>
   );
 }
