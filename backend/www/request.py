3642d55e535e85967b0d4b4018e5e8dfe3c66ccc
Will Gleason <wgleason9@gmail.com>
2023-04-11 08:56:11 -0700

    more progress towards planetscale migration

--- backend/www/request.py
+++ backend/www/request.py
@@ -35,46 +35,23 @@ class Request:
             if pyllica_records := await pyllica_wrapper.get(
                 self.ticket, on_no_records_found=self.set_no_records
             ):
-
-                def clean_csv_row(value):
-                    if value is None:
-                        return r"\N"
-                    return str(value).replace("|", "\\|")
-
-                csv_file_like_object = io.StringIO()
-                for record in pyllica_records:
-                    row = (
-                        record.year,
-                        record.month,
-                        record.day,
-                        record.term,
-                        self.id,
-                        record.count,
-                    )
-                    csv_file_like_object.write("|".join(map(clean_csv_row, row)) + "\n")
-
-                csv_file_like_object.seek(0)
                 with db_conn.cursor() as curs:
-                    file_path = "/path/to/your/file.csv"
-                    table_name = "groupcounts"
+                    print("Inserting records")
+                    curs.executemany(
+                        """INSERT INTO groupcounts (year, month, searchterm, requestid, count)
+                    VALUES (%s, %s, %s, %s, %s)""",
+                        [
+                            (
+                                record.year,
+                                record.month,
+                                record.term,
+                                self.id,
+                                record.count,
+                            )
+                            for record in pyllica_records
+                        ],
+                    )
 
-                    # Define the columns and separator used in the file
-                    columns = ("year", "month", "day", "searchterm", "requestid", "count")
-                    separator = "|"
-
-                    # Define the LOAD DATA INFILE statement
-                    query = f"""
-                        LOAD DATA LOCAL INFILE '{file_path}'
-                        INTO TABLE {table_name}
-                        FIELDS TERMINATED BY '{separator}'
-                        ENCLOSED BY '\"'
-                        LINES TERMINATED BY '\\n'
-                        IGNORE 1 ROWS
-                        ({', '.join(columns)})
-                    """
-
-                    # Execute the query
-                    curs.execute(query)
             if self.state not in ["too_many_records", "no_records"]:
                 self.state = "completed"
             self.on_update_progress(
