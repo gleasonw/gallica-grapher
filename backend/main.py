682a50a6d7d5c40abb27efef04dac8add30583ed
Will Gleason <wgleason9@gmail.com>
2023-04-11 10:27:42 -0700

    change port

--- backend/main.py
+++ backend/main.py
@@ -161,4 +161,4 @@ def graph_data(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
