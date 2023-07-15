c877183ef7b1ccda5dd9abd46b109cacbb468f45
Will Gleason <will.gleason@novadiscovery.com>
2023-07-15 12:40:21 +0200

    remove unused files, change web url

--- backend/launch_backend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-source venv311/bin/activate
-uvicorn main:app --reload
--- frontend/src/components/apiURL.ts
+++ frontend/src/components/apiURL.ts
@@ -1,6 +1,6 @@
 export let apiURL: string;
 if (process.env.NODE_ENV === "development") {
-  apiURL = "https://gallica-web.ew.r.appspot.com";
+  apiURL = "https://gallica-grapher-production-6518.up.railway.app/";
 } else {
-  apiURL = "https://gallica-web.ew.r.appspot.com";
+  apiURL = "https://gallica-grapher-production-6518.up.railway.app/";
 }
