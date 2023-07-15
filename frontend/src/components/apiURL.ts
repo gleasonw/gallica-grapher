ed74f76cff21bfad0a169dcfda80b98844d4b3ba
Will Gleason <will.gleason@novadiscovery.com>
2023-07-15 12:49:54 +0200

    change url

--- frontend/src/components/apiURL.ts
+++ frontend/src/components/apiURL.ts
@@ -1,6 +1,6 @@
 export let apiURL: string;
 if (process.env.NODE_ENV === "development") {
-  apiURL = "https://gallica-grapher-production-6518.up.railway.app/";
+  apiURL = "https://gallica-grapher-production-6518.up.railway.app";
 } else {
-  apiURL = "https://gallica-grapher-production-6518.up.railway.app/";
+  apiURL = "https://gallica-grapher-production-6518.up.railway.app";
 }
