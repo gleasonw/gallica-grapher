d941ce8e6d26d760f814f459eefca20d284d1fb0
Will Gleason <will.gleason@novadiscovery.com>
2023-07-15 12:06:38 +0200

    latest

--- frontend/src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-import { InitialData } from "../app/page";
-
-export default function App({
-  result,
-  staticRecords,
-  staticSeries,
-  staticRecordParams,
-  staticSeriesParams,
-}: InitialData);
