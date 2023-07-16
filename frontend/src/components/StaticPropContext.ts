892dfe847f5bc6867a7be5d329fdccb69076939c
Will Gleason <will.gleason@novadiscovery.com>
2023-07-16 11:04:25 +0200

    latest

--- frontend/src/components/StaticPropContext.ts
+++
@@ -1,7 +0,0 @@
-import { InferGetStaticPropsType } from "next";
-import { getStaticProps } from "../pages";
-import { createContext } from "react";
-
-export const StaticPropContext = createContext<
-  InferGetStaticPropsType<typeof getStaticProps> | undefined
->(undefined);
