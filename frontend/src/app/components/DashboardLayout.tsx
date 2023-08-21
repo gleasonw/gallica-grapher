f017975d37ddf38580b38ffc216c6b82327106e5
Will Gleason <wgleason9@gmail.com>
2023-08-21 08:32:09 -0700

    remove unused components

--- frontend/src/app/components/DashboardLayout.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardLayout(props: { children: React.ReactNode }) {
-  return (
-    <div className="ml-10 mr-10">
-      <div className="justify-center items-center flex flex-col mt-10 gap-5">
-        {props.children}
-      </div>
-    </div>
-  );
-}
