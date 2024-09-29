b9c55c69e3ed6f04f5f1d5da864a7080bd7634cb
gleasonw <wgleason9@gmail.com>
2024-09-29 17:24:10 +0200

    use internal railway url

--- src/app/components/apiURL.ts
+++ src/app/components/apiURL.ts
@@ -2,5 +2,6 @@ export let apiURL: string;
 if (process.env.NODE_ENV === "development") {
   apiURL = "http://localhost:8000";
 } else {
-  apiURL = "https://gallica-proxy-production.up.railway.app";
+  apiURL =
+    process.env.API_URL ?? "https://gallica-proxy-production.up.railway.app";
 }
