72edcda6031e21eeeb0965be79b0630eb9c804d1
gleasonw <wgleason9@gmail.com>
2024-09-29 10:00:30 +0200

    update base url

--- src/app/components/apiURL.ts
+++ src/app/components/apiURL.ts
@@ -2,5 +2,5 @@ export let apiURL: string;
 if (process.env.NODE_ENV === "development") {
   apiURL = "http://localhost:8000";
 } else {
-  apiURL = "https://gallica-getter-little-snow-3158.fly.dev";
+  apiURL = "https://gallica-proxy-production.up.railway.app";
 }
