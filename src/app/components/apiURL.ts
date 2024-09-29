f36e4a925ce05139c6ad82efd55da16e7708227f
gleasonw <wgleason9@gmail.com>
2024-09-29 17:24:44 +0200

    revert

--- src/app/components/apiURL.ts
+++ src/app/components/apiURL.ts
@@ -2,6 +2,5 @@ export let apiURL: string;
 if (process.env.NODE_ENV === "development") {
   apiURL = "http://localhost:8000";
 } else {
-  apiURL =
-    process.env.API_URL ?? "https://gallica-proxy-production.up.railway.app";
+  apiURL = "https://gallica-proxy-production.up.railway.app";
 }
