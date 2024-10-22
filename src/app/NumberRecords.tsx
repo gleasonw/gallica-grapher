4fa17dcdf586e1dda3b14820b289abab2ff06c0b
gleasonw <wgleason9@gmail.com>
2024-10-22 19:48:19 +0200

    guard against navigator weirdness

--- src/app/NumberRecords.tsx
+++ src/app/NumberRecords.tsx
@@ -1,5 +1,8 @@
 "use client";
 
 export function NumberRecords({ records }: { records: number }) {
+  if (typeof window === "undefined") {
+    return null;
+  }
   return <>{Intl.NumberFormat(navigator.language).format(records)} résultats</>;
 }
